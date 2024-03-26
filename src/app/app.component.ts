01c67c3fac09c0fcbf06333ab64f4aae77972de2
mara <ms.vergnaud@gmail.com>
2024-03-26 11:57:26 +0100

    architecture faite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+// import { LoginService } from './services/login.service';
+// import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Equipe-Restaurant-front';
+  title = 'Equipe Restaurant Front';
+
+  // public isAuthenticated$: Observable<boolean>;
+
+  constructor( 
+    // private loginService: LoginService
+    ) {
+    // this.isAuthenticated$ = loginService.isAuthenticated$;
+  }
 }
