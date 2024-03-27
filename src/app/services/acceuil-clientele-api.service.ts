2d10494325c388511a6a726f258199fec49e6803
Florian <florian.faneuil@gmail.com>
2024-03-27 11:21:45 +0100

    modif url tables restaurant par ID

--- src/app/services/acceuil-clientele-api.service.ts
+++ src/app/services/acceuil-clientele-api.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, of } from 'rxjs';
-import { Table, Tables } from '../entities/table';
+import { Tables } from '../entities/table';
 
 @Injectable({ providedIn: 'root' })
 export class AcceuilClienteleAPIService {
   constructor(private httpClient: HttpClient) {}
-  URL: string = 'http://localhost:8080/table';
+  URL: string = 'http://localhost:8080/table/'; /* ON AFFICHE LES TABLES DU
+  RESTAURANT ID4 CAR POUR LE  PathRestaurant/4 EN DUR CAR PAS ENCORE GESTION*/
 
   public getAllTables(): Observable<Tables> {
-    return this.httpClient.get<Tables>(this.URL).pipe(
+    return this.httpClient.get<Tables>(this.URL+'PathRestaurant/4').pipe(
       map((tables: Tables | null) => {
         if (tables == null) {
           return [];
