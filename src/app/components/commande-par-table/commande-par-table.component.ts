bce3ffd4839ff09c799ee8fb77b297f5f4e65b06
Melien Pierre <pimerors@gmail.com>
2024-03-29 09:32:55 +0100

    ajout de plat

--- src/app/components/commande-par-table/commande-par-table.component.ts
+++ src/app/components/commande-par-table/commande-par-table.component.ts
@@ -1,3 +1,5 @@
+
+import { ServicePlatAPIService } from './../../services/service-plat-api.service';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import {  Table } from '../../entities/table';
@@ -5,6 +7,7 @@ import { CommonModule } from '@angular/common';
 import { OnInit } from '@angular/core';
 import { ServiceEnSalleAPIService } from '../../services/service-en-salle-api.service';
 import { ActivatedRoute } from '@angular/router';
+import { Plats,Plat } from '../../entities/plat';
 
 
 @Component({
@@ -17,16 +20,31 @@ import { ActivatedRoute } from '@angular/router';
 export class CommandeParTableComponent  implements OnInit {
   
   public tables$?: Observable<Table>;
+  public plats$?: Observable<Plats>;
+  public platsSelectionnes!: Plats
 
   constructor(public serviceEnSalleAPIService: ServiceEnSalleAPIService,
-    private route: ActivatedRoute,){}
+    private route: ActivatedRoute,public servicePlatAPIService: ServicePlatAPIService){}
 
   ngOnInit(): void {
+    this.platsSelectionnes  = []
     const tableId = this.route.snapshot.params['id'];
     this.tables$ = this.serviceEnSalleAPIService.getTableById(tableId);
+    this.plats$ = this.servicePlatAPIService.getAllTables();
     console.log(" test",this.tables$);
     
   }
-  
+
+  ajouterPlatACommande(plat: Plat|null): void {
+    if (plat) {
+      this.platsSelectionnes.push(plat);
+    }
+  }
+  supprimerPlatDeCommande(plat: Plat): void {
+    const index = this.platsSelectionnes.indexOf(plat);
+    if (index !== -1) {
+      this.platsSelectionnes.splice(index, 1);
+    }
+  }
 
 }
