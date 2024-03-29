c1f42ef75e29925822899d7b31d6f4da53283d32
mara <ms.vergnaud@gmail.com>
2024-03-29 09:36:38 +0100

    devut front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,13 @@ import {
   RouterStateSnapshot,
   Routes,
 } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { AccueilClienteleComponent } from './components/accueil-clientele/accueil-clientele.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
 import { ServiceEnSalleComponent } from './components/service-en-salle/service-en-salle.component';
 import { CuisineComponent } from './components/cuisine/cuisine.component';
 import { CaisseComponent } from './components/caisse/caisse.component';
 import { FormReservationComponent } from './components/form-reservation/form-reservation.component';
+import { HomeComponent } from './components/home/home.component';
 // import { NewStructuralDirectivesComponent } from './components/new-structural-directives/new-structural-directives.component';
 // import { inject } from '@angular/core';
 // import { StorageService } from './services/storage.service';
@@ -37,8 +37,7 @@ export const routes: Routes = [
   { path: 'service-en-salle', component: ServiceEnSalleComponent },
   { path: 'cuisine', component: CuisineComponent },
   { path: 'caisse', component: CaisseComponent },
-  { path: 'form-reservation', component: FormReservationComponent},
-
+  { path: 'form-reservation', component: FormReservationComponent },
 
   // {
   //     path: '**',
